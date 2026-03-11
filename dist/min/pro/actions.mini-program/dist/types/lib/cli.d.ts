b99af9e3694d13c315c1e49f2331906a399bcc89
likaiqiang <likaiqiang.frontend@gmail.com>
2026-03-11 19:58:20 +0800

    build dist/min

---
+++ dist/min/pro/actions.mini-program/dist/types/lib/cli.d.ts
@@ -0,0 +1,11 @@
+export default class Cli {
+    options: any;
+    constructor(options: Object);
+    preview(): Promise<void>;
+    upload(): Promise<void>;
+    cloud(): Promise<void>;
+    pack_npm(): Promise<void>;
+    get_dev_source_map(): Promise<void>;
+    pack_npm_manually(): Promise<void>;
+    custom(): Promise<void>;
+}
